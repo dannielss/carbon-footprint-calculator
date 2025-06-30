49c846fffc369a65b1ca2b3447067e3120a9567e
Daniel Sousa <daniel_sousa.p@outlook.com>
2025-06-30 00:16:12 -0300

    feat: add rate limit, cors and more

--- backend/src/infrastructure/graphql/server.ts
+++ backend/src/infrastructure/graphql/server.ts
@@ -1,13 +1,21 @@
 import { readFileSync } from "node:fs";
 import path from "node:path";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
 import { Resolvers } from "./resolvers";
 import { IServer } from "@/domain/interfaces";
 import { useCases } from "@/usecases";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@as-integrations/express5";
+import rateLimit from "express-rate-limit";
 
 export class ApolloGraphQLServer implements IServer {
   async start(port = 4000): Promise<void> {
+    const app = express();
+    const httpServer = http.createServer(app);
+
     const typeDefs = readFileSync(
       path.join(__dirname, "schema.graphql"),
       "utf-8"
@@ -19,12 +27,37 @@ export class ApolloGraphQLServer implements IServer {
     const server = new ApolloServer({
       typeDefs,
       resolvers,
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     });
 
-    const { url } = await startStandaloneServer(server, {
-      listen: { port },
-    });
+    await server.start();
 
-    console.log(`🚀  Server ready at: ${url}`);
+    app.use(
+      "/graphql",
+      cors(),
+      express.json(),
+      rateLimit({
+        windowMs: 60 * 1000,
+        max: 30,
+        standardHeaders: true,
+        legacyHeaders: false,
+        message: {
+          error: {
+            message: "Too many requests, please try again later.",
+          },
+        },
+        handler: (_, res) => {
+          res.status(429).json({
+            data: null,
+            error: {
+              message: "Too many requests, please try again later.",
+            },
+          });
+        },
+      }),
+      expressMiddleware(server)
+    );
+    await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
+    console.log(`🚀 Server ready at http://localhost:4000/graphql`);
   }
 }
