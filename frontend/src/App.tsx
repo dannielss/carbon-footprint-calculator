bf61399d2cc6851450a5912b29fcaa6f5ee6eda9
Daniel Sousa <daniel_sousa.p@outlook.com>
2025-06-30 16:47:48 -0300

    feat: configure graphql

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,62 +3,33 @@
 import type React from "react";
 
 import { useState } from "react";
-import type { FormData } from "./types/formData";
-import FirstStep from "./components/firstStep";
-import SecondStep from "./components/secondStep";
+import { FirstStep, SecondStep } from "./components";
+import { useForm } from "./hooks";
+import useData from "./hooks/useData";
 
 export default function App() {
   const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState<FormData>({
-    zipCode: "",
-    householdSize: 1,
-    housing: {
-      electricityKWhPerMonth: 0,
-      fuelOilGallonsPerMonth: 0,
-      lpgGallonsPerMonth: 0,
-      naturalGasThermsPerMonth: 0,
-      wasteLbsPerMonth: 0,
-      waterGallonsPerMonth: 0,
-    },
-    travel: {
-      busMilesPerMonth: 0,
-      flyingMilesPerMonth: 0,
-      metroMilesPerMonth: 0,
-      railMilesPerMonth: 0,
-      taxiMilesPerMonth: 0,
-      vehicleMilesPerMonth: 0,
-    },
-  });
+  const [start, setStart] = useState(false);
+  const {
+    formData,
+    setFormData,
+    updateHousingData,
+    updateTravelData,
+    handleClearForm,
+  } = useForm();
+  const { data, error } = useData(formData, start);
 
   const handleBasicInfoSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (formData.zipCode && formData.householdSize > 0) {
       setStep(2);
+      setStart(true);
     }
   };
 
-  const updateHousingData = (
-    field: keyof FormData["housing"],
-    value: number
-  ) => {
-    setFormData((prev) => ({
-      ...prev,
-      housing: {
-        ...prev.housing,
-        [field]: value,
-      },
-    }));
-  };
-
-  const updateTravelData = (field: keyof FormData["travel"], value: number) => {
-    setFormData((prev) => ({
-      ...prev,
-      travel: {
-        ...prev.travel,
-        [field]: value,
-      },
-    }));
-  };
+  if (error) {
+    return <div>error...</div>;
+  }
 
   if (step === 1) {
     return (
@@ -74,8 +45,11 @@ export default function App() {
     <SecondStep
       formData={formData}
       setStep={setStep}
+      setStart={setStart}
       updateHousingData={updateHousingData}
       updateTravelData={updateTravelData}
+      data={data}
+      handleClearForm={handleClearForm}
     />
   );
 }
